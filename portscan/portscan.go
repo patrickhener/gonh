3867b13cdbdc97d440f22b101f348d41ea629843
Patrick Hener <patrick.hener@to.com>
2022-04-11 08:45:50 +0200

    added nessus tcp scanner to portscan mode

--- portscan/portscan.go
+++ portscan/portscan.go
@@ -12,7 +12,7 @@ Nessus UDP scanner (ID: 34277)
 
 // Do will do it
 func Do(c *nessus.Collection, template string, stdout bool) (string, error) {
-	q := "pluginid=11219,34277"
+	q := "pluginid=11219,34277,10335"
 	_, rstring, err := query.Do(q, c, template, stdout)
 
 	return rstring, err
