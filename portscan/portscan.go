4723457bf2bb31816264dbcdd838fa5cf120c07b
Patrick Hener <patrick.hener@to.com>
2022-11-03 11:55:37 +0100

    added 'snmp scanner' to mode 'portscan'

--- portscan/portscan.go
+++ portscan/portscan.go
@@ -8,12 +8,13 @@ import (
 /*
 Nessus SYN scanner (ID: 11219)
 Nessus UDP scanner (ID: 34277)
+Nessus SNMP scanner (ID: 14274)
 */
 
 // Do will do it
 func Do(c *nessus.Collection, template string, stdout bool) (string, error) {
 	// added 'and not port=0 to prevent from 0/udp in list'
-	q := "pluginid=11219,34277,10335 and not port=0"
+	q := "pluginid=11219,34277,10335,14274 and not port=0"
 	_, rstring, err := query.Do(q, c, template, stdout)
 
 	return rstring, err
